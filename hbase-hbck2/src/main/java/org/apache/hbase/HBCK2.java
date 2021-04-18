3e0480ef3660029d3d90cad7710888b4379a662c
Peter Somogyi <psomogyi@apache.org>
2021-04-18 14:57:24 +0200

    HBASE-25659 [hbck2] Add hbase-operator-tools command for scheduleSCPsForUnknownServers

--- hbase-hbck2/src/main/java/org/apache/hbase/HBCK2.java
+++ hbase-hbck2/src/main/java/org/apache/hbase/HBCK2.java
@@ -99,6 +99,7 @@ public class HBCK2 extends Configured implements org.apache.hadoop.util.Tool {
   private static final String VERSION = "version";
   private static final String SET_REGION_STATE = "setRegionState";
   private static final String SCHEDULE_RECOVERIES = "scheduleRecoveries";
+  private static final String RECOVER_UNKNOWN = "recoverUnknown";
   private static final String GENERATE_TABLE_INFO = "generateMissingTableDescriptorFile";
   private static final String FIX_META = "fixMeta";
   // TODO update this map in case of the name of a method changes in Hbck interface
@@ -109,7 +110,9 @@ public class HBCK2 extends Configured implements org.apache.hadoop.util.Tool {
               put(SET_TABLE_STATE, Arrays.asList("setTableStateInMeta"));
               put(BYPASS, Arrays.asList("bypassProcedure"));
               put(SCHEDULE_RECOVERIES, Arrays.asList("scheduleServerCrashProcedure",
-                      "scheduleServerCrashProcedures")); }});
+                      "scheduleServerCrashProcedures"));
+              put(RECOVER_UNKNOWN, Arrays.asList("scheduleSCPsForUnknownServers"));
+          }});
 
   private static final String ADD_MISSING_REGIONS_IN_META_FOR_TABLES =
     "addFsRegionsMissingInMeta";
@@ -384,6 +387,10 @@ public class HBCK2 extends Configured implements org.apache.hadoop.util.Tool {
     return hbck.scheduleServerCrashProcedure(serverNames);
   }
 
+  List<Long> recoverUnknown(Hbck hbck) throws IOException {
+    return hbck.scheduleSCPsForUnknownServers();
+  }
+
   private HBaseProtos.ServerName parseServerName(String serverName) {
     ServerName sn = ServerName.parseServerName(serverName);
     return HBaseProtos.ServerName.newBuilder().setHostName(sn.getHostname()).
@@ -429,6 +436,8 @@ public class HBCK2 extends Configured implements org.apache.hadoop.util.Tool {
     usageSetTableState(writer);
     writer.println();
     usageScheduleRecoveries(writer);
+    writer.println();
+    usageRecoverUnknown(writer);
     writer.println();
     usageUnassigns(writer);
     writer.println();
@@ -657,6 +666,15 @@ public class HBCK2 extends Configured implements org.apache.hadoop.util.Tool {
     writer.println("   Returns the pid(s) of the created ServerCrashProcedure(s) or -1 if");
     writer.println("   no procedure created (see master logs for why not).");
     writer.println("   Command support added in hbase versions 2.0.3, 2.1.2, 2.2.0 or newer.");
+  }
+
+  private static void usageRecoverUnknown(PrintWriter writer) {
+    writer.println(" " + RECOVER_UNKNOWN);
+    writer.println("   Schedule ServerCrashProcedure(SCP) RegionServers that are reported");
+    writer.println("   as unknown.");
+    writer.println("   Returns the pid(s) of the created ServerCrashProcedure(s) or -1 if");
+    writer.println("   no procedure created (see master logs for why not).");
+    writer.println("   Command support added in hbase versions 2.2.7, 2.3.5, 2.4.3, 2.5.0 or newer.");
   }
 
   private static void usageUnassigns(PrintWriter writer) {
@@ -891,6 +909,17 @@ public class HBCK2 extends Configured implements org.apache.hadoop.util.Tool {
         try (ClusterConnection connection = connect(); Hbck hbck = connection.getHbck()) {
           checkFunctionSupported(connection, command);
           System.out.println(toString(scheduleRecoveries(hbck, purgeFirst(commands))));
+        }
+        break;
+
+      case RECOVER_UNKNOWN:
+        if (commands.length > 1) {
+          showErrorMessage(command + " doesn't take any arguments");
+          return EXIT_FAILURE;
+        }
+        try (ClusterConnection connection = connect(); Hbck hbck = connection.getHbck()) {
+          checkFunctionSupported(connection, command);
+          System.out.println(toString(recoverUnknown(hbck)));
         }
         break;
 
