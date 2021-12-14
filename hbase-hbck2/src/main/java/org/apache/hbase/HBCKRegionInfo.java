2ce1e2c231de89bf6024510c9d977431deaa2397
Josh Elser <jelser@cloudera.com>
2021-12-14 18:02:38 -0500

    HBASE-26656 Utility to correct corrupt RegionInfo's in hbase:meta
    A standalone utility which corrects hbase:meta given the problem
    described by HBASE-23328. Includes the ability to both "report" corrupt
    regions as well as correct them. This tool will ensure that other
    HBCK2 utilities continue to work without additional modification.
    
    Signed-off-by: Peter Somogyi <psomogyi@apache.org>
    
    Closes #102

---
+++ hbase-hbck2/src/main/java/org/apache/hbase/HBCKRegionInfo.java
@@ -0,0 +1,150 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hbase;
+
+
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.client.RegionInfo;
+import org.apache.hadoop.hbase.util.ByteArrayHashKey;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.HashKey;
+import org.apache.hadoop.hbase.util.JenkinsHash;
+
+/**
+ * A copy of utilities from {@link org.apache.hadoop.hbase.client.RegionInfo} to
+ * copy internal methods into HBCK2 for stability to avoid using Private methods.
+ */
+public final class HBCKRegionInfo {
+
+  /**
+   * Separator used to demarcate the encodedName in a region name
+   * in the new format. See description on new format above.
+   */
+  static final int ENC_SEPARATOR = '.';
+
+  static final int MD5_HEX_LENGTH = 32;
+
+  static final int DEFAULT_REPLICA_ID = 0;
+
+  static final byte REPLICA_ID_DELIMITER = (byte)'_';
+
+  private HBCKRegionInfo() {}
+
+  /**
+   * Does region name contain its encoded name?
+   * @param regionName region name
+   * @return boolean indicating if this a new format region
+   *         name which contains its encoded name.
+   */
+  public static boolean hasEncodedName(final byte[] regionName) {
+    // check if region name ends in ENC_SEPARATOR
+    return (regionName.length >= 1) &&
+      (regionName[regionName.length - 1] == RegionInfo.ENC_SEPARATOR);
+  }
+
+  /**
+   * @return the encodedName
+   */
+  public static String encodeRegionName(final byte [] regionName) {
+    String encodedName;
+    if (hasEncodedName(regionName)) {
+      // region is in new format:
+      // <tableName>,<startKey>,<regionIdTimeStamp>/encodedName/
+      encodedName = Bytes.toString(regionName,
+      regionName.length - MD5_HEX_LENGTH - 1,
+      MD5_HEX_LENGTH);
+    } else {
+      // old format region name. First hbase:meta region also
+      // use this format.EncodedName is the JenkinsHash value.
+      HashKey<byte[]> key = new ByteArrayHashKey(regionName, 0, regionName.length);
+      int hashVal = Math.abs(JenkinsHash.getInstance().hash(key, 0));
+      encodedName = String.valueOf(hashVal);
+    }
+    return encodedName;
+  }
+
+  /**
+   * Separate elements of a regionName.
+   * Region name is of the format:
+   * <code>tablename,startkey,regionIdTimestamp[_replicaId][.encodedName.]</code>.
+   * Startkey can contain the delimiter (',') so we parse from the start and then parse from
+   * the end.
+   * @return Array of byte[] containing tableName, startKey and id OR null if not parseable
+   *   as a region name.
+   */
+  public static byte [][] parseRegionNameOrReturnNull(final byte[] regionName) {
+    int offset = -1;
+    for (int i = 0; i < regionName.length; i++) {
+      if (regionName[i] == HConstants.DELIMITER) {
+        offset = i;
+        break;
+      }
+    }
+    if (offset == -1) {
+      return null;
+    }
+    byte[] tableName = new byte[offset];
+    System.arraycopy(regionName, 0, tableName, 0, offset);
+    offset = -1;
+
+    int endOffset = regionName.length;
+    // check whether regionName contains encodedName
+    if (regionName.length > MD5_HEX_LENGTH + 2 &&
+        regionName[regionName.length-1] == ENC_SEPARATOR &&
+        regionName[regionName.length-MD5_HEX_LENGTH-2] == ENC_SEPARATOR) {
+      endOffset = endOffset - MD5_HEX_LENGTH - 2;
+    }
+
+    // parse from end
+    byte[] replicaId = null;
+    int idEndOffset = endOffset;
+    for (int i = endOffset - 1; i > 0; i--) {
+      if (regionName[i] == REPLICA_ID_DELIMITER) { //replicaId may or may not be present
+        replicaId = new byte[endOffset - i - 1];
+        System.arraycopy(regionName, i + 1, replicaId, 0,
+          endOffset - i - 1);
+        idEndOffset = i;
+        // do not break, continue to search for id
+      }
+      if (regionName[i] == HConstants.DELIMITER) {
+        offset = i;
+        break;
+      }
+    }
+    if (offset == -1) {
+      return null;
+    }
+    byte [] startKey = HConstants.EMPTY_BYTE_ARRAY;
+    if(offset != tableName.length + 1) {
+      startKey = new byte[offset - tableName.length - 1];
+      System.arraycopy(regionName, tableName.length + 1, startKey, 0,
+        offset - tableName.length - 1);
+    }
+    byte [] id = new byte[idEndOffset - offset - 1];
+    System.arraycopy(regionName, offset + 1, id, 0,
+      idEndOffset - offset - 1);
+    byte [][] elements = new byte[replicaId == null ? 3 : 4][];
+    elements[0] = tableName;
+    elements[1] = startKey;
+    elements[2] = id;
+    if (replicaId != null) {
+      elements[3] = replicaId;
+    }
+    return elements;
+  }
+}
---
+++ hbase-hbck2/src/main/java/org/apache/hbase/RegionInfoMismatchTool.java
@@ -0,0 +1,182 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hbase;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.RegionInfo;
+import org.apache.hadoop.hbase.client.RegionInfoBuilder;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.exceptions.DeserializationException;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hbase.HBCKMetaTableAccessor.MetaScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Standalone utility to correct the bug corrected by HBASE-23328 in which
+ * the region name in the rowkey of a row in meta does not match the name
+ * which is stored in the value of the info:regioninfo column of the same row.
+ */
+public class RegionInfoMismatchTool {
+  private static final Logger LOG = LoggerFactory.getLogger(RegionInfoMismatchTool.class);
+
+  private final Connection connection;
+
+  public RegionInfoMismatchTool(Connection connection) {
+    this.connection = connection;
+  }
+
+  static class MalformedRegion {
+    byte[] regionName;
+    RegionInfo regionInfo;
+
+    MalformedRegion(byte[] regionName, RegionInfo regionInfo) {
+      this.regionName = regionName;
+      this.regionInfo = regionInfo;
+    }
+
+    byte[] getRegionName() {
+      return regionName;
+    }
+
+    RegionInfo getRegionInfo() {
+      return regionInfo;
+    }
+
+    @Override
+    public String toString() {
+      return "regionName=" + Bytes.toStringBinary(regionName) + ", regioninfo="
+        + regionInfo.toString();
+    }
+  }
+
+  /**
+   * Returns a list of {@link MalformedRegion}'s which exist in meta. If there are
+   * no malformed regions, the returned list will be empty.
+   */
+  List<MalformedRegion> getMalformedRegions() throws IOException {
+    try (Table meta = connection.getTable(TableName.META_TABLE_NAME)) {
+      MetaScanner<MalformedRegion> scanner = new MetaScanner<>();
+      return scanner.scanMeta(connection,
+        scan -> scan.addFamily(HConstants.CATALOG_FAMILY),
+        r -> {
+          Cell riCell = r.getColumnLatestCell(HConstants.CATALOG_FAMILY,
+              HConstants.REGIONINFO_QUALIFIER);
+          RegionInfo info = RegionInfo.parseFromOrNull(riCell.getValueArray(),
+              riCell.getValueOffset(), riCell.getValueLength());
+          // Get the expected value from the RegionInfo in the cell value
+          byte[] valueEncodedRegionName = info.getEncodedNameAsBytes();
+          // Compare that to what is actually in the rowkey
+          HBCKMetaTableAccessor.getMetaKeyForRegion(info);
+          byte[] rowKeyRegionName = CellUtil.cloneRow(riCell);
+          byte[] rowkeyEncodedRegionName = Bytes.toBytes(
+              HBCKRegionInfo.encodeRegionName(rowKeyRegionName));
+          // If they are equal, we are good.
+          if (Arrays.equals(rowkeyEncodedRegionName, valueEncodedRegionName)) {
+            // Returning null will cause `scanMeta` to ignore this row
+            LOG.debug("Ignoring region {} because rowkey aligns with value", info);
+            return null;
+          }
+
+          LOG.debug("Found mismatched region {} and {}", Bytes.toStringBinary(rowKeyRegionName),
+              Bytes.toStringBinary(valueEncodedRegionName));
+          // Only return row/regioninfo pairs that are wrong
+          return new MalformedRegion(rowKeyRegionName, info);
+        });
+    }
+  }
+
+  /**
+   * Run the RegionInfoMistmatchTool. Use the {@code fix} argument to control whether this method
+   * will report problems or fix problems.
+   *
+   * @param fix True if hbase:meta should be updated. False to report on any problems.
+   */
+  public void run(boolean fix) throws IOException, DeserializationException {
+    run(System.out, fix);
+  }
+
+  void run(PrintStream out, boolean fix) throws IOException, DeserializationException {
+    List<MalformedRegion> regionsToFix = getMalformedRegions();
+    if (!fix) {
+      out.println("Fix mode is disabled, printing all malformed regions detected:");
+      for (MalformedRegion r : regionsToFix) {
+        out.println("Rowkey " + HBCKRegionInfo.encodeRegionName(r.getRegionName())
+            + " does not match " + r.getRegionInfo());
+      }
+    }
+    out.println("Found " + regionsToFix.size() + " regions to fix.");
+    try (Table meta = connection.getTable(TableName.META_TABLE_NAME)) {
+      for (MalformedRegion regionToFix : regionsToFix) {
+        final byte[] regionName = regionToFix.getRegionName();
+        final RegionInfo wrongRegionInfo = regionToFix.getRegionInfo();
+
+        // The encoded region name is an MD5 hash, but the regionID is what is actually
+        // broken by HBASE-23328
+        byte[][] regionNameParts = HBCKRegionInfo.parseRegionNameOrReturnNull(regionName);
+        if (regionNameParts == null) {
+          throw new RuntimeException("Couldn't parse parts from "
+              + Bytes.toStringBinary(regionName));
+        }
+        int i = 0;
+        for (byte[] part : regionNameParts) {
+          LOG.debug("Region name part[{}]: {}", i++, Bytes.toStringBinary(part));
+        }
+        // Third component of a region name is just a literal numeric (not a binary-encoded long)
+        long regionId = Long.parseLong(Bytes.toString(regionNameParts[2]));
+        RegionInfo correctedRegionInfo = RegionInfoBuilder.newBuilder(wrongRegionInfo)
+            .setRegionId(regionId)
+            .setReplicaId(0)
+            .build();
+
+        String rowkeyEncodedRegionName = HBCKRegionInfo.encodeRegionName(regionName);
+        String updatedValueEncodedRegionName = correctedRegionInfo.getEncodedName();
+        if (!rowkeyEncodedRegionName.equals(updatedValueEncodedRegionName)) {
+          out.println("Aborting: sanity-check failed on updated RegionInfo. Expected encoded "
+              + "region name " +rowkeyEncodedRegionName + " but got "
+              + updatedValueEncodedRegionName + ".");
+          out.println("Incorrectly created RegionInfo was: " + correctedRegionInfo);
+          throw new RuntimeException("Failed sanity-check on corrected RegionInfo");
+        }
+
+        out.println("Updating RegionInfo for " + Bytes.toStringBinary(regionName) + " to "
+            + correctedRegionInfo);
+
+        // Write the update back to meta.
+        if (fix) {
+          meta.put(HBCKMetaTableAccessor.makePutFromRegionInfo(correctedRegionInfo,
+                System.currentTimeMillis()));
+        }
+      }
+      if (!fix) {
+        out.println("Fix mode is not enabled, hbase:meta was not updated. See the tool output for"
+            + " a list of detected problematic regions. Re-run the tool without the dry run option"
+            + " to persist updates to hbase:meta.");
+      }
+    }
+  }
+}
